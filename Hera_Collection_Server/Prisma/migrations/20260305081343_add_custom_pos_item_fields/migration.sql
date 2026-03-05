0060b8c2620790cde40fd2859a9377f904df83f5
Francis3560 <kushkiddeny11@gmail.com>
2026-03-05 00:39:05 -0800

    corrected naming mismatch in Cart table

--- Hera_Collection_Server/Prisma/migrations/20260305081343_add_custom_pos_item_fields/migration.sql
+++ Hera_Collection_Server/Prisma/migrations/20260305081343_add_custom_pos_item_fields/migration.sql
@@ -1,7 +1,7 @@
 -- AlterTable
-ALTER TABLE `cartitem` ADD COLUMN `customTitle` VARCHAR(160) NULL,
+ALTER TABLE `CartItem` ADD COLUMN `customTitle` VARCHAR(160) NULL,
     ADD COLUMN `price` DECIMAL(10, 2) NULL,
     MODIFY `productId` INTEGER NULL;
 
 -- AlterTable
-ALTER TABLE `orderitem` MODIFY `productId` INTEGER NULL;
+ALTER TABLE `OrderItem` MODIFY `productId` INTEGER NULL;
